523fd1d3bc85a2562dca087d73f09b0f2d84cb4b
Anton Shevchuk <antoni.y.shevchuk@gmail.com>
2023-12-28 03:21:40 +0000

    compatibility with groovy 3.0 enabled

--- src/main/java/groovyx/net/http/thirdparty/GAEClientConnection.java
+++ src/main/java/groovyx/net/http/thirdparty/GAEClientConnection.java
@@ -21,266 +21,261 @@
 
 package groovyx.net.http.thirdparty;
 
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.TimeUnit;
+import com.google.appengine.api.urlfetch.*;
 import org.apache.http.*;
-import org.apache.http.conn.*;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.ManagedClientConnection;
 import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.message.BasicHttpResponse;
-import org.apache.http.params.*;
-import org.apache.http.protocol.*;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 
-import com.google.appengine.api.urlfetch.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
 
-class GAEClientConnection
-  implements ManagedClientConnection {
+class GAEClientConnection implements ManagedClientConnection {
 
-  public GAEClientConnection(ClientConnectionManager cm, HttpRoute route, Object state) {
-    this.connManager = cm;
-    this.route = route;
-    this.state = state;
-    this.closed = true;
-  }
-
-  // From interface ManagedClientConnection
-
-  public boolean isSecure() {
-    return route.isSecure();
-  }
-
-  public HttpRoute getRoute() {
-    return route;
-  }
-
-  public javax.net.ssl.SSLSession getSSLSession() {
-    return null;
-  }
-
-  public void open(HttpRoute route, HttpContext context, HttpParams params)
-    throws IOException {
-    close();
-    this.route = route;
-//     System.err.println(">>>>");
-  }
-
-  public void tunnelTarget(boolean secure, HttpParams params)
-    throws IOException {
-    throw new IOException("tunnelTarget() not supported");
-  }
-
-  public void tunnelProxy(HttpHost next, boolean secure, HttpParams params)
-    throws IOException {
-    throw new IOException("tunnelProxy() not supported");
-  }
-
-  public void layerProtocol(HttpContext context, HttpParams params)
-    throws IOException {
-    throw new IOException("layerProtocol() not supported");
-  }
-
-  public void markReusable() {
-    reusable = true;
-  }
-
-  public void unmarkReusable() {
-    reusable = false;
-  }
-
-  public boolean isMarkedReusable() {
-    return reusable;
-  }
-
-  public void setState(Object state) {
-    this.state = state;
-  }
-
-  public Object getState() {
-    return state;
-  }
-
-  public void setIdleDuration(long duration, TimeUnit unit) {
-    // Do nothing
-  }
-
-
-  // From interface HttpClientConnection
-
-  public boolean isResponseAvailable(int timeout)
-    throws IOException {
-    return response != null;
-  }
-
-
-  public void sendRequestHeader(HttpRequest request)
-    throws HttpException, IOException {
-    try {
-      HttpHost host = route.getTargetHost();
-
-      URI uri = new URI(host.getSchemeName()
-            + "://"
-            + host.getHostName()
-            + ((host.getPort() == -1) ? "" : (":" + host.getPort()))
-            + request.getRequestLine().getUri());
-
-      this.request = new HTTPRequest(uri.toURL(),
-                     HTTPMethod.valueOf(request.getRequestLine().getMethod()),
-                     FetchOptions.Builder.disallowTruncate().doNotFollowRedirects());
-    }
-    catch (URISyntaxException ex) {
-      throw new IOException("Malformed request URI: " + ex.getMessage(), ex);
-    }
-    catch (IllegalArgumentException ex) {
-      throw new IOException("Unsupported HTTP method: " + ex.getMessage(), ex);
+    public GAEClientConnection(ClientConnectionManager cm, HttpRoute route, Object state) {
+        this.connManager = cm;
+        this.route = route;
+        this.state = state;
+        this.closed = true;
     }
 
-//     System.err.println("SEND: " + this.request.getMethod() + " " + this.request.getURL());
+    // From interface ManagedClientConnection
 
-    for (Header h : request.getAllHeaders()) {
-//       System.err.println("SEND: " + h.getName() + ": " + h.getValue());
-      this.request.addHeader(new HTTPHeader(h.getName(), h.getValue()));
-    }
-  }
-
-
-  public void sendRequestEntity(HttpEntityEnclosingRequest request)
-    throws HttpException, IOException {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    if (request.getEntity() != null) {
-      request.getEntity().writeTo(baos);
-    }
-    this.request.setPayload(baos.toByteArray());
-  }
-
-
-  public HttpResponse receiveResponseHeader()
-    throws HttpException, IOException {
-    if (this.response == null) {
-      flush();
+    public boolean isSecure() {
+        return route.isSecure();
     }
 
-    HttpResponse response = new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1),
-                          this.response.getResponseCode(),
-                          null);
-//     System.err.println("RECV: " + response.getStatusLine());
-
-    for (HTTPHeader h : this.response.getHeaders()) {
-//       System.err.println("RECV: " + h.getName() + ": " + h.getValue());
-      response.addHeader(h.getName(), h.getValue());
+    public HttpRoute getRoute() {
+        return route;
     }
 
-    return response;
-  }
-
-
-  public void receiveResponseEntity(HttpResponse response)
-    throws HttpException, IOException {
-    if (this.response == null) {
-      throw new IOException("receiveResponseEntity() called on closed connection");
+    @Override
+    public String getId() {
+        return null;
     }
 
-    ByteArrayEntity bae = new ByteArrayEntity(this.response.getContent());
-    bae.setContentType(response.getFirstHeader("Content-Type"));
-    response.setEntity(bae);
+    @Override
+    public void bind(Socket socket) throws IOException {
 
-    response = null;
-  }
-
-  public void flush()
-    throws IOException {
-    if (request != null) {
-      try {
-//  System.err.println("----");
-    response = urlFS.fetch(request);
-    request = null;
-      }catch (IOException ex) {
-    ex.printStackTrace();
-    throw ex;
-      }
     }
-    else {
-      response = null;
+
+    @Override
+    public Socket getSocket() {
+        return null;
     }
-  }
 
+    public javax.net.ssl.SSLSession getSSLSession() {
+        return null;
+    }
 
-  // From interface HttpConnection
+    public void open(HttpRoute route, HttpContext context, HttpParams params) throws IOException {
+        close();
+        this.route = route;
+        // System.err.println(">>>>");
+    }
 
-  public void close()
-    throws IOException {
-    request  = null;
-    response = null;
-    closed   = true;
-//     System.err.println("<<<<");
-  }
+    public void tunnelTarget(boolean secure, HttpParams params) throws IOException {
+        throw new IOException("tunnelTarget() not supported");
+    }
 
-  public boolean isOpen() {
-    return request != null || response != null;
-  }
+    public void tunnelProxy(HttpHost next, boolean secure, HttpParams params) throws IOException {
+        throw new IOException("tunnelProxy() not supported");
+    }
 
-  public boolean isStale() {
-    return !isOpen() && !closed;
-  }
+    public void layerProtocol(HttpContext context, HttpParams params) throws IOException {
+        throw new IOException("layerProtocol() not supported");
+    }
 
-  public void setSocketTimeout(int timeout) {
-  }
+    public void markReusable() {
+        reusable = true;
+    }
 
-  public int getSocketTimeout() {
-    return -1;
-  }
+    public void unmarkReusable() {
+        reusable = false;
+    }
 
-  public void shutdown()
-    throws IOException {
-    close();
-  }
+    public boolean isMarkedReusable() {
+        return reusable;
+    }
 
-  public HttpConnectionMetrics getMetrics() {
-    return null;
-  }
+    public void setState(Object state) {
+        this.state = state;
+    }
 
+    public Object getState() {
+        return state;
+    }
 
-  // From interface HttpInetConnection
+    public void setIdleDuration(long duration, TimeUnit unit) {
+        // Do nothing
+    }
 
-  public InetAddress getLocalAddress() {
-    return null;
-  }
+    // From interface HttpClientConnection
 
-  public int getLocalPort() {
-    return 0;
-  }
+    public boolean isResponseAvailable(int timeout) throws IOException {
+        return response != null;
+    }
 
-  public InetAddress getRemoteAddress() {
-    return null;
-  }
+    public void sendRequestHeader(HttpRequest request) throws HttpException, IOException {
+        try {
+            HttpHost host = route.getTargetHost();
 
-  public int getRemotePort() {
-    HttpHost host = route.getTargetHost();
-    return connManager.getSchemeRegistry().getScheme(host).resolvePort(host.getPort());
-  }
+            URI uri = new URI(host.getSchemeName()
+                    + "://"
+                    + host.getHostName()
+                    + ((host.getPort() == -1) ? "" : (":" + host.getPort()))
+                    + request.getRequestLine().getUri());
 
+            this.request = new HTTPRequest(uri.toURL(),
+                    HTTPMethod.valueOf(request.getRequestLine().getMethod()),
+                    FetchOptions.Builder.disallowTruncate().doNotFollowRedirects());
+        } catch (URISyntaxException ex) {
+            throw new IOException("Malformed request URI: " + ex.getMessage(), ex);
+        } catch (IllegalArgumentException ex) {
+            throw new IOException("Unsupported HTTP method: " + ex.getMessage(), ex);
+        }
 
-  // From interface ConnectionReleaseTrigger
+        // System.err.println("SEND: " + this.request.getMethod() + " " +
+        // this.request.getURL());
 
-  public void releaseConnection()
-    throws IOException {
-    connManager.releaseConnection(this, Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-  }
+        for (Header h : request.getAllHeaders()) {
+            // System.err.println("SEND: " + h.getName() + ": " + h.getValue());
+            this.request.addHeader(new HTTPHeader(h.getName(), h.getValue()));
+        }
+    }
 
-  public void abortConnection()
-    throws IOException {
-    unmarkReusable();
-    shutdown();
-  }
+    public void sendRequestEntity(HttpEntityEnclosingRequest request) throws HttpException, IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        if (request.getEntity() != null) {
+            request.getEntity().writeTo(baos);
+        }
+        this.request.setPayload(baos.toByteArray());
+    }
 
-  private ClientConnectionManager connManager;
-  private HttpRoute route;
-  private Object state;
-  private boolean reusable;
+    public HttpResponse receiveResponseHeader() throws HttpException, IOException {
+        if (this.response == null) {
+            flush();
+        }
 
-  private HTTPRequest request;
-  private HTTPResponse response;
-  private boolean closed;
+        HttpResponse response = new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1),
+                this.response.getResponseCode(),
+                null);
+        // System.err.println("RECV: " + response.getStatusLine());
 
-  private static URLFetchService urlFS = URLFetchServiceFactory.getURLFetchService();
+        for (HTTPHeader h : this.response.getHeaders()) {
+            // System.err.println("RECV: " + h.getName() + ": " + h.getValue());
+            response.addHeader(h.getName(), h.getValue());
+        }
+
+        return response;
+    }
+
+    public void receiveResponseEntity(HttpResponse response) throws HttpException, IOException {
+        if (this.response == null) {
+            throw new IOException("receiveResponseEntity() called on closed connection");
+        }
+
+        ByteArrayEntity bae = new ByteArrayEntity(this.response.getContent());
+        bae.setContentType(response.getFirstHeader("Content-Type"));
+        response.setEntity(bae);
+
+        response = null;
+    }
+
+    public void flush() throws IOException {
+        if (request != null) {
+            try {
+                // System.err.println("----");
+                response = urlFS.fetch(request);
+                request = null;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                throw ex;
+            }
+        } else {
+            response = null;
+        }
+    }
+
+    // From interface HttpConnection
+
+    public void close() throws IOException {
+        request = null;
+        response = null;
+        closed = true;
+        // System.err.println("<<<<");
+    }
+
+    public boolean isOpen() {
+        return request != null || response != null;
+    }
+
+    public boolean isStale() {
+        return !isOpen() && !closed;
+    }
+
+    public void setSocketTimeout(int timeout) {
+    }
+
+    public int getSocketTimeout() {
+        return -1;
+    }
+
+    public void shutdown() throws IOException {
+        close();
+    }
+
+    public HttpConnectionMetrics getMetrics() {
+        return null;
+    }
+
+    // From interface HttpInetConnection
+
+    public InetAddress getLocalAddress() {
+        return null;
+    }
+
+    public int getLocalPort() {
+        return 0;
+    }
+
+    public InetAddress getRemoteAddress() {
+        return null;
+    }
+
+    public int getRemotePort() {
+        HttpHost host = route.getTargetHost();
+        return connManager.getSchemeRegistry().getScheme(host).resolvePort(host.getPort());
+    }
+
+    // From interface ConnectionReleaseTrigger
+
+    public void releaseConnection() throws IOException {
+        connManager.releaseConnection(this, Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+    }
+
+    public void abortConnection() throws IOException {
+        unmarkReusable();
+        shutdown();
+    }
+
+    private ClientConnectionManager connManager;
+    private HttpRoute route;
+    private Object state;
+    private boolean reusable;
+
+    private HTTPRequest request;
+    private HTTPResponse response;
+    private boolean closed;
+
+    private static URLFetchService urlFS = URLFetchServiceFactory.getURLFetchService();
 }
